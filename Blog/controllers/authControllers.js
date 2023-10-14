330aa872485b5db3b275e3ff05cf8587aa8e1939
Esteban Martinez <e.martineza126@gmail.com>
2023-10-14 17:00:45 -0300

    actulizacion authController

--- Blog/controllers/authControllers.js
+++ Blog/controllers/authControllers.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 // ...
 exports.autenticarUsuario = passport.authenticate('local', {
-  successRedirect: '/',
+  successRedirect: '/administracion',
   failureRedirect: '/iniciar-sesion',
 });
