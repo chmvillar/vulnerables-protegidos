554c5225500cf3921748abb659abec81d444c196
Esteban Martinez <e.martineza126@gmail.com>
2023-11-05 16:48:55 -0300

    estilos
    modificafiones en los estilos

--- Blog/controllers/homeControllers.js
+++ Blog/controllers/homeControllers.js
@@ -1,4 +1,9 @@
+const Post = require('../models/Post');
+
 exports.home = (req, res) => {
+
+    const arreglo = [];
+    arreglo.push( Post.findAll({}));
     res.render('home', {
         nombrePagina : 'Menu Principal'
     })
