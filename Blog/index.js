84a299f2bfb65ce0d8c2cb50b34191b5bd918654
Esteban Martinez <e.martineza126@gmail.com>
2023-11-16 18:28:05 -0300

    creando el servidor

--- Blog/index.js
+++ Blog/index.js
@@ -64,6 +64,10 @@ app.use((req, res, next) => {
 // Routing
 app.use('/', router());
 
-app.listen(process.env.PORT, () => {
+const host = process.env.HOST || '0.0.0.0';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
   console.log('¡Está vivo!');
 });
